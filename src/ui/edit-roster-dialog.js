31b5298bf4266bc53f14e9a74217b5f9ec7f7cdd
Niet <niet@pokefarm.com>
2025-09-14 14:33:46 +0100

    Improve clarity of teams in roster dialog

--- src/ui/edit-roster-dialog.js
+++ src/ui/edit-roster-dialog.js
@@ -14,19 +14,30 @@ function makeDialog() {
 	dialog.innerHTML = `
 		<form method="dialog" class="modal">
 			<h3>${t('editRoster.title')}</h3>
-			<div class="grid" style="grid-template-columns: 1fr 1fr 160px;"></div>
+			<div class="editroster-container"></div>
 			<footer>
 				<button value="cancel">${t('cancel')}</button>
 				<button value="save" type="button" class="btn--primary">${t('save')}</button>
 			</footer>
 		</form>
 	`;
-	const grid = /** @type {HTMLDivElement} */(dialog.querySelector('div.grid'));
+	const container = /** @type {HTMLDivElement} */(dialog.querySelector('div.editroster-container'));
 	const save = /** @type {HTMLButtonElement} */(dialog.querySelector('button[value=save]'));
 	const cancel = /** @type {HTMLButtonElement} */(dialog.querySelector('button[value=cancel]'));
 	document.body.append(dialog);
 	dialog.addEventListener('close', () => dialog.remove());
-	return { dialog, grid, save, cancel };
+	return { dialog, container, save, cancel };
+}
+
+/**
+ * @param {HTMLElement} container
+ */
+function createGridHost(container) {
+	const grid = document.createElement('div');
+	grid.classList.add('grid');
+	grid.style.gridTemplateColumns = '1fr 1fr 160px';
+	container.append(grid);
+	return grid;
 }
 
 /**
@@ -100,9 +111,10 @@ function createPlayerRow(grid, player) {
  * @param {Mogi} mogi
  */
 export function openEditRoster(mogi) {
-	const { dialog, grid, save, cancel } = makeDialog();
-
+	const { dialog, container, save, cancel } = makeDialog();
+	
 	if (mogi.playersPerTeam === 1) {
+		const grid = createGridHost(container);
 		createHeaderRow(grid);
 		// Build player rows
 		for (const player of mogi.roster) {
@@ -112,28 +124,34 @@ export function openEditRoster(mogi) {
 	else {
 		// Build team rows
 		for (const team of mogi.teams) {
+			const grid = createGridHost(container);
 			createTeamRow(grid, team);
 			createHeaderRow(grid);
 			for (const player of team.players) {
 				createPlayerRow(grid, player);
 			}
+			grid.style.background = `linear-gradient(to bottom, ${TEAM_COLOURS[team.index]}80 16px, ${TEAM_COLOURS[team.index]}40 48px)`;
 		}
 
-		grid.addEventListener('input', e => {
+		container.addEventListener('input', e => {
 			const select = /** @type {HTMLElement} */(e.target).closest("select");
 			if (select && select.name === 'index' && select.value !== select.dataset.previousValue) {
 				// if another select has this value, switch their values
-				const otherSelect = [...grid.querySelectorAll('select')].find(s => s !== select && s.name === select.name && s.value === select.value);
+				const otherSelect = [...container.querySelectorAll('select')].find(s => s !== select && s.name === select.name && s.value === select.value);
 				if (otherSelect && select.dataset.previousValue) {
 					otherSelect.dataset.previousValue = otherSelect.value = select.dataset.previousValue;
+					const grid = /** @type {HTMLDivElement} */(otherSelect.closest('div.grid'));
+					if( grid) grid.style.background = `linear-gradient(to bottom, ${TEAM_COLOURS[+otherSelect.value]}80 16px, ${TEAM_COLOURS[+otherSelect.value]}40 48px)`;
 				}
 				// in all cases, update previous value
 				select.dataset.previousValue = select.value;
+				const grid = /** @type {HTMLDivElement} */(select.closest('div.grid'));
+				if( grid) grid.style.background = `linear-gradient(to bottom, ${TEAM_COLOURS[+select.value]}80 16px, ${TEAM_COLOURS[+select.value]}40 48px)`;
 			}
 		});
 	}
 
-	grid.addEventListener('click', e => {
+	container.addEventListener('click', e => {
 		const button = /** @type {HTMLElement} */(e.target).closest("button");
 		if (button && button.dataset.subPlayerId) {
 			const p = mogi.roster.byId(button.dataset.subPlayerId);
@@ -145,7 +163,7 @@ export function openEditRoster(mogi) {
 	});
 
 	save.addEventListener('click', () => {
-		const formFields = [...grid.querySelectorAll('input'), ...grid.querySelectorAll('select')];
+		const formFields = [...container.querySelectorAll('input'), ...container.querySelectorAll('select')];
 
 		// Edit roster
 		formFields.forEach(input => {
